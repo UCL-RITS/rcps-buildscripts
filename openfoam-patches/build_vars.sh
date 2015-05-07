d8a9ea0293e547e82427fdd7c27b63ec8a7ba136
Owain Kenway <o.kenway@ucl.ac.uk>
2015-05-07 15:48:03 +0100

    Script to automate variable setting

---
+++ openfoam-patches/build_vars.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script re-sets some variables that keep getting unset by the various 
+# OpenFOAM config scripts.
+
+# QT variables
+export QTDIR=${INSTALL_PREFIX}/ThirdParty-${THIRDPARTY_VERSION}/platforms/linux64Icc/qt-${QT_VERSION}
+export QTINC=${INSTALL_PREFIX}/ThirdParty-${THIRDPARTY_VERSION}/platforms/linux64Icc/qt-${QT_VERSION}/include
+export QTLIB=${INSTALL_PREFIX}/ThirdParty-${THIRDPARTY_VERSION}/platforms/linux64Icc/qt-${QT_VERSION}/lib
+export QT_DIR=${INSTALL_PREFIX}/ThirdParty-${THIRDPARTY_VERSION}/platforms/linux64Icc/qt-${QT_VERSION}
+
+# Fix PATH
+export PATH=${INSTALL_PREFIX}/ThirdParty-${THIRDPARTY_VERSION}/platforms/linux64Icc/qt-${QT_VERSION}/bin:$PATH
+
+# Fix LD_LIBRARY_PATH - otherwise libstdc++ breaks
+export LD_LIBRARY_PATH=/shared/ucl/apps/gcc/4.9.2/lib64:${LD_LIBRARY_PATH}
