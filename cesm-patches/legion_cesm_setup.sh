c10b2edf8ca713c518e5f33019ef2e86f36f53f0
Owain Kenway <o.kenway@ucl.ac.uk>
2015-06-10 11:21:58 +0100

    Initial import of settings from old Legion.
    Getting CESM to build will involve reverse-engineering what I did three years ago.  These are the files I added in to CESM 1.0.4 on the old Legion OS.

---
+++ cesm-patches/legion_cesm_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script sets up some useful directories within a Legion user's scratch space.
+# Owain Kenway, 24th of May, 2012
+
+usercesm=~/Scratch/cesm
+
+mkdir -p $usercesm/archive
+mkdir -p $usercesm/cases
+mkdir -p $usercesm/inputdata
+mkdir -p $usercesm/output
+mkdir -p $usercesm/scratch
