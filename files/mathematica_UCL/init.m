0b753597d546f6a9be027051c5fe4540fa3c54e0
balston <b.alston@ucl.ac.uk>
2015-07-20 11:53:37 +0100

    UCL Mathematica config files.

---
+++ files/mathematica_UCL/init.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+Begin["System`Private`"]
+(* Hide any symbols which are created *)
+
+protected = Unprotect[$ProcessorCount]
+$ProcessorCount = ToExpression [Environment["OMP_NUM_THREADS"]]
+protected = Protect [$ProcessorCount]
+
+WriteString [$Output, "Mathematica running on Legion at UCL"]
+
+End[]
