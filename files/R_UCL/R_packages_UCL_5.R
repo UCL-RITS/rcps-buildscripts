acb382cbc796babf170b9f9c9283cdfecb20b11b
balston <b.alston@ucl.ac.uk>
2017-05-15 11:19:14 +0100

    R and Bioconductor: adding package rmarkdown.

--- files/R_UCL/R_packages_UCL_5.R
+++ files/R_UCL/R_packages_UCL_5.R
@@ -140,6 +140,9 @@ v8conf <- 'INCLUDE_DIR=/shared/ucl/apps/v8/3.15/v8/include LIB_DIR=/shared/ucl/a
 install.packages ("V8", lib=mainLib, repos=repros, configure.vars=v8conf);
 install.packages ("tmap", lib=mainLib, repos=repros);
 
+# For Lucia Conde (l.conde@ucl.ac.uk) May 2017
+install.packages ("rmarkdown", lib=mainLib, repos=repros);
+
 # End of R_packages_UCL_5
 
 # install.packages ("XXX", lib=mainLib, repos=repros);
