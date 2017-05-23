404d11864d57d16d60cd25a3dc2e25dc364106fb
balston <b.alston@ucl.ac.uk>
2017-05-23 11:26:57 +0100

    R and Bioconductor: update OpenMPI links for 1.10.1.

--- files/R_UCL/R_packages_MPI_1.R
+++ files/R_UCL/R_packages_MPI_1.R
@@ -3,6 +3,8 @@
 # Install additional R packages used used for MPI support.
 #
 # June 2016
+#
+# Uodated may 2017 for OpenMPI 1.10.1
 
 #=====================================================================
 #  R_packages_MPI_1
@@ -17,7 +19,7 @@ mainLib;
 dbLib;
 repros;
 
-install.packages ("Rmpi", lib=mainLib, repos=repros, configure.args="--with-Rmpi-include=/shared/ucl/apps/openmpi/1.8.4/gnu-4.9.2/include --with-Rmpi-libpath=/shared/ucl/apps/openmpi/1.8.4/gnu-4.9.2/lib --with-Rmpi-type=OPENMPI");
+install.packages ("Rmpi", lib=mainLib, repos=repros, configure.args="--with-Rmpi-include=/shared/ucl/apps/openmpi/1.10.1/gnu-4.9.2/include --with-Rmpi-libpath=/shared/ucl/apps/openmpi/1.10.1/gnu-4.9.2/lib --with-Rmpi-type=OPENMPI");
 install.packages ("snow", lib=mainLib, repos=repros);
 
 
