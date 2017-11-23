c6b2920791e537e9995f0d108fbc6603463a97bf
Owain Kenway <o.kenway@ucl.ac.uk>
2017-11-23 11:25:48 +0000

    Cudnn install.

---
+++ cudnn-5.1_install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+###############################################
+# Installing 
+#
+# by Owain Kenway, 2017
+#
+
+NAME=${NAME:-cudnn}
+VERSION=${VERSION:-5.1}
+CUDA_VERSION=${CUDA_VERSION:-8.0}
+INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$CUDA_VERSION/$COMPILER_TAG}
+
+SRC_ARCHIVE=${SRC_ARCHIVE:-/shared/ucl/apps/cudnn/installers/cudnn-${CUDA_VERSION}-linux-x86-v${VERSION}.tgz}
+
+set -e
+
+mkdir -p $INSTALL_PREFIX
+cd $INSTALL_PREFIX
+
+tar -xvf $archive
