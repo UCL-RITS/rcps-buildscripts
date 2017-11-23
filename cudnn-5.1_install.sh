7ba4b38ccf01505cd682245597b9fb5fd80bff5e
Owain Kenway <o.kenway@ucl.ac.uk>
2017-11-23 11:29:29 +0000

    x86 not x86

--- cudnn-5.1_install.sh
+++ cudnn-5.1_install.sh
@@ -11,7 +11,7 @@ VERSION=${VERSION:-5.1}
 CUDA_VERSION=${CUDA_VERSION:-8.0}
 INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$CUDA_VERSION/$COMPILER_TAG}
 
-SRC_ARCHIVE=${SRC_ARCHIVE:-/shared/ucl/apps/cudnn/installers/cudnn-${CUDA_VERSION}-linux-x86-v${VERSION}.tgz}
+SRC_ARCHIVE=${SRC_ARCHIVE:-/shared/ucl/apps/cudnn/installers/cudnn-${CUDA_VERSION}-linux-x64-v${VERSION}.tgz}
 
 set -e
 
