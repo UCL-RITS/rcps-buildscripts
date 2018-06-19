634de721e4748dbfa7a29d10e2f95b9a46666823
Brian Alston <b.alston@ucl.ac.uk>
2018-06-19 10:51:54 +0100

    R 3.5.0 MPI packages script updated to use mpi/openmpi/3.0.0/gnu-4.9.2.

--- files/R_UCL/R_packages_MPI_1.R
+++ files/R_UCL/R_packages_MPI_1.R
@@ -4,7 +4,9 @@
 #
 # June 2016
 #
-# Uodated may 2017 for OpenMPI 1.10.1
+# Uodated May 2017 for OpenMPI 1.10.1
+# Updated June 2018 to fix MPI support in R 3.5.0 - now using
+#         mpi/openmpi/3.0.0/gnu-4.9.2
 
 #=====================================================================
 #  R_packages_MPI_1
@@ -19,7 +21,7 @@ mainLib;
 dbLib;
 repros;
 
-install.packages ("Rmpi", lib=mainLib, repos=repros, configure.args="--with-Rmpi-include=/shared/ucl/apps/openmpi/1.10.1/gnu-4.9.2/include --with-Rmpi-libpath=/shared/ucl/apps/openmpi/1.10.1/gnu-4.9.2/lib --with-Rmpi-type=OPENMPI");
+install.packages ("Rmpi", lib=mainLib, repos=repros, configure.args="--with-Rmpi-include=/shared/ucl/apps/openmpi/3.0.0/gnu-4.9.2/include --with-Rmpi-libpath=/shared/ucl/apps/openmpi/3.0.0/gnu-4.9.2/lib --with-Rmpi-type=OPENMPI");
 install.packages ("snow", lib=mainLib, repos=repros);
 
 
