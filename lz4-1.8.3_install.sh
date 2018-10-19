1012828367b9561ba586c48e98bdd93a840196a1
Owain Kenway <o.kenway@ucl.ac.uk>
2018-10-19 15:56:56 +0100

    lz4 - required by vg

---
+++ lz4-1.8.3_install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+###############################################
+# Installing 
+#
+# by Owain Kenway, 2017
+#
+
+COMPILER_TAG=${COMPILER_TAG:-gnu-4.9.2}
+NAME=${NAME:-lz4}
+VERSION=${VERSION:-1.8.3}
+INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$COMPILER_TAG}
+SRC_ARCHIVE=${SRC_ARCHIVE:-https://github.com/lz4/lz4.git#tag=v${VERSION}}
+
+set -e
+
+mkdir -p /dev/shm/${NAME}
+
+temp_dir=`mktemp -d -p /dev/shm/${NAME}`
+
+cd $temp_dir
+
+git clone $SRC_ARCHIVE
+
+cd ${NAME}
+
+mkdir -p $INSTALL_PREFIX
+make PREFIX=$INSTALL_PREFIX
+make install PREFIX=$INSTALL_PREFIX
+
+rm -rf $temp_dir
