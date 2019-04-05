72d923f0626eb4e6be4d80adbc9dcfa219b73430
Brian Alston <b.alston@ucl.ac.uk>
2019-04-05 14:07:47 +0100

    Bioconductor install script updated for 3.8 - now using BiocManager::install.

--- files/R_UCL/Bioconductor_UCL_1.R
+++ files/R_UCL/Bioconductor_UCL_1.R
@@ -15,194 +15,196 @@ mainLib;
 dbLib;
 repros;
 
-# Download the Bioconductor installer and install the base packages
+# Download the Bioconductor installer and install the base package
 
 if (!requireNamespace ("BiocManager"))
     install.packages ("BiocManager", repos=repros)
 BiocManager::install ()
 
+# Install Bioconductor packages
+
 BiocManager::install ("Biobase", type="source", lib=mainLib);
+BiocManager::install ("qvalue", type="source", lib=mainLib);
+BiocManager::install ("IRanges", type="source", lib=mainLib);
+BiocManager::install ("GenomeInfoDb", type="source", lib=mainLib);
+BiocManager::install ("AnnotationDbi", type="source", lib=mainLib);
+BiocManager::install ("annotate", type="source", lib=mainLib);
+BiocManager::install ("biomaRt", type="source", lib=mainLib);
+BiocManager::install ("XVector", type="source", lib=mainLib);
+BiocManager::install ("zlibbioc", type="source", lib=mainLib);
+BiocManager::install ("Biostrings", type="source", lib=mainLib);
+BiocManager::install ("DynDoc", type="source", lib=mainLib);
+BiocManager::install ("affyio", type="source", lib=mainLib);
+BiocManager::install ("preprocessCore", type="source", lib=mainLib);
+BiocManager::install ("BiocInstaller", type="source", lib=mainLib);
+BiocManager::install ("affy", type="source", lib=mainLib);
+BiocManager::install ("gcrma", type="source", lib=mainLib);
+BiocManager::install ("affyPLM", type="source", lib=mainLib);
+BiocManager::install ("genefilter", type="source", lib=mainLib);
+BiocManager::install ("simpleaffy", type="source", lib=mainLib);
+BiocManager::install ("affyQCReport", type="source", lib=mainLib);
+BiocManager::install ("affydata", type="source", lib=mainLib);
 
-## biocLite (pkgs="qvalue", type="source", lib=mainLib);
-## biocLite (pkgs="IRanges", type="source", lib=mainLib);
-## biocLite (pkgs="GenomeInfoDb", type="source", lib=mainLib);
-## biocLite (pkgs="AnnotationDbi", type="source", lib=mainLib);
-## biocLite (pkgs="annotate", type="source", lib=mainLib);
-## biocLite (pkgs="biomaRt", type="source", lib=mainLib);
-## biocLite (pkgs="XVector", type="source", lib=mainLib);
-## biocLite (pkgs="zlibbioc", type="source", lib=mainLib);
-## biocLite (pkgs="Biostrings", type="source", lib=mainLib);
-## biocLite (pkgs="DynDoc", type="source", lib=mainLib);
-## biocLite (pkgs="affyio", type="source", lib=mainLib);
-## biocLite (pkgs="preprocessCore", type="source", lib=mainLib);
-## biocLite (pkgs="BiocInstaller", type="source", lib=mainLib);
-## biocLite (pkgs="affy", type="source", lib=mainLib);
-## biocLite (pkgs="gcrma", type="source", lib=mainLib);
-## biocLite (pkgs="affyPLM", type="source", lib=mainLib);
-## biocLite (pkgs="genefilter", type="source", lib=mainLib);
-## biocLite (pkgs="simpleaffy", type="source", lib=mainLib);
-## biocLite (pkgs="affyQCReport", type="source", lib=mainLib);
-## biocLite (pkgs="affydata", type="source", lib=mainLib);
+BiocManager::install ("GO.db", type="source", lib=dbLib);
+BiocManager::install ("KEGG.db", type="source", lib=dbLib);
 
-## biocLite (pkgs="GO.db", type="source", lib=dbLib);
-## biocLite (pkgs="KEGG.db", type="source", lib=dbLib);
+BiocManager::install ("annaffy", type="source", lib=mainLib);
+BiocManager::install ("geneplotter", type="source", lib=mainLib);
 
-## biocLite (pkgs="annaffy", type="source", lib=mainLib);
-## biocLite (pkgs="geneplotter", type="source", lib=mainLib);
+BiocManager::install ("org.Hs.eg.db", type="source", lib=dbLib);
+BiocManager::install ("hgu95av2.db", type="source", lib=dbLib);
 
-## biocLite (pkgs="org.Hs.eg.db", type="source", lib=dbLib);
-## biocLite (pkgs="hgu95av2.db", type="source", lib=dbLib);
+BiocManager::install ("limma", type="source", lib=mainLib);
+BiocManager::install ("marray", type="source", lib=mainLib);
+BiocManager::install ("multtest", type="source", lib=mainLib);
+BiocManager::install ("vsn", type="source", lib=mainLib);
 
-## biocLite (pkgs="limma", type="source", lib=mainLib);
-## biocLite (pkgs="marray", type="source", lib=mainLib);
-## biocLite (pkgs="multtest", type="source", lib=mainLib);
-## biocLite (pkgs="vsn", type="source", lib=mainLib);
+# For Cristian Taccioli
+#
+# xps needs ROOT. New version (>= v5.34.05) needed for Bioconductor 2.12
 
-## # For Cristian Taccioli
-## #
-## # xps needs ROOT. New version (>= v5.34.05) needed for Bioconductor 2.12
+BiocManager::install ("xps", type="source", lib=mainLib);
+BiocManager::install ("affylmGUI", type="source", lib=mainLib);
+BiocManager::install ("widgetTools", type="source", lib=mainLib);
 
-## biocLite (pkgs="xps", type="source", lib=mainLib);
-## biocLite (pkgs="affylmGUI", type="source", lib=mainLib);
-## biocLite (pkgs="widgetTools", type="source", lib=mainLib);
+# For Sakshi Gulati - added October 2012
 
-## # For Sakshi Gulati - added October 2012
+BiocManager::install ("GenomicRanges", type="source", lib=mainLib);
+BiocManager::install ("Rsamtools", type="source", lib=mainLib);
+BiocManager::install ("BSgenome", type="source", lib=mainLib);
+BiocManager::install ("BSgenome.Hsapiens.UCSC.hg19", type="source", lib=mainLib);
+BiocManager::install ("BiocParallel", type="source", lib=mainLib);
+BiocManager::install ("GenomicAlignments", type="source", lib=mainLib);
+BiocManager::install ("rtracklayer", type="source", lib=mainLib);
+BiocManager::install ("made4", type="source", lib=mainLib);
+BiocManager::install ("graph", type="source", lib=mainLib);
+BiocManager::install ("GSEABase", type="source", lib=mainLib);
 
-## biocLite (pkgs="GenomicRanges", type="source", lib=mainLib);
-## biocLite (pkgs="Rsamtools", type="source", lib=mainLib);
-## biocLite (pkgs="BSgenome", type="source", lib=mainLib);
-## biocLite (pkgs="BSgenome.Hsapiens.UCSC.hg19", type="source", lib=mainLib);
-## biocLite (pkgs="BiocParallel", type="source", lib=mainLib);
-## biocLite (pkgs="GenomicAlignments", type="source", lib=mainLib);
-## biocLite (pkgs="rtracklayer", type="source", lib=mainLib);
-## biocLite (pkgs="made4", type="source", lib=mainLib);
-## biocLite (pkgs="graph", type="source", lib=mainLib);
-## biocLite (pkgs="GSEABase", type="source", lib=mainLib);
+BiocManager::install ("hugene10stprobeset.db", type="source", lib=dbLib);
+BiocManager::install ("hugene10sttranscriptcluster.db", type="source", lib=dbLib);
 
-## biocLite (pkgs="hugene10stprobeset.db", type="source", lib=dbLib);
-## biocLite (pkgs="hugene10sttranscriptcluster.db", type="source", lib=dbLib);
+# For Somdutta Dhir (somdutta.dhir@ucl.ac.uk) - added February 2013
 
-## # For Somdutta Dhir (somdutta.dhir@ucl.ac.uk) - added February 2013
+BiocManager::install ("GenomeGraphs", type="source", lib=mainLib);
+BiocManager::install ("GenomicFeatures", type="source", lib=mainLib);
 
-## biocLite (pkgs="GenomeGraphs", type="source", lib=mainLib);
-## biocLite (pkgs="GenomicFeatures", type="source", lib=mainLib);
+# Removed from Bioconductor 3.5
+BiocManager::install ("VariantAnnotation", type="source", lib=mainLib);
 
-## # Removed from Bioconductor 3.5
-## biocLite (pkgs="VariantAnnotation", type="source", lib=mainLib);
+BiocManager::install ("biovizBase", type="source", lib=mainLib);
+BiocManager::install ("Gviz", type="source", lib=mainLib);
+BiocManager::install ("annmap", type="source", lib=mainLib);
 
-## biocLite (pkgs="biovizBase", type="source", lib=mainLib);
-## biocLite (pkgs="Gviz", type="source", lib=mainLib);
-## biocLite (pkgs="annmap", type="source", lib=mainLib);
+# For Adam Levine (a.levine@ucl.ac.uk) - added August 2013
 
-## # For Adam Levine (a.levine@ucl.ac.uk) - added August 2013
+BiocManager::install ("impute", type="source", lib=mainLib);
+install.packages ("WGCNA", lib=mainLib, repos=repros);
 
-## biocLite (pkgs="impute", type="source", lib=mainLib);
-## install.packages ("WGCNA", lib=mainLib, repos=repros);
+# For Dr Gioia Altobelli g.altobelli@ucl.ac.uk - added Oct 2013 updated Mar 2014
 
-## # For Dr Gioia Altobelli g.altobelli@ucl.ac.uk - added Oct 2013 updated Mar 2014
+BiocManager::install ("edgeR", type="source", lib=mainLib);
+BiocManager::install ("BeadDataPackR", type="source", lib=mainLib);
+BiocManager::install ("illuminaio", type="source", lib=mainLib);
+BiocManager::install ("beadarray", type="source", lib=mainLib);
+BiocManager::install ("beadarrayExampleData", type="source", lib=mainLib);
 
-## biocLite (pkgs="edgeR", type="source", lib=mainLib);
-## biocLite (pkgs="BeadDataPackR", type="source", lib=mainLib);
-## biocLite (pkgs="illuminaio", type="source", lib=mainLib);
-## biocLite (pkgs="beadarray", type="source", lib=mainLib);
-## biocLite (pkgs="beadarrayExampleData", type="source", lib=mainLib);
+BiocManager::install ("AnnotationForge", type="source", lib=dbLib);
+BiocManager::install ("illuminaHumanv4.db", type="source", lib=dbLib);
 
-## biocLite (pkgs="AnnotationForge", type="source", lib=dbLib);
-## biocLite (pkgs="illuminaHumanv4.db", type="source", lib=dbLib);
+# For Antonia Ford (a.ford.11@ucl.ac.uk) - added Jan 2014
 
-## # For Antonia Ford (a.ford.11@ucl.ac.uk) - added Jan 2014
+BiocManager::install ("seqLogo", type="source", lib=mainLib);
 
-## biocLite (pkgs="seqLogo", type="source", lib=mainLib);
+# For Michela Leonardi (m.leonardi@ucl.ac.uk) - added June 2014
 
-## # For Michela Leonardi (m.leonardi@ucl.ac.uk) - added June 2014
+BiocManager::install ("RBGL", type="source", lib=mainLib);
+BiocManager::install ("Category", type="source", lib=mainLib);
+BiocManager::install ("GOstats", type="source", lib=mainLib);
 
-## biocLite (pkgs="RBGL", type="source", lib=mainLib);
-## biocLite (pkgs="Category", type="source", lib=mainLib);
-## biocLite (pkgs="GOstats", type="source", lib=mainLib);
+# For Javier Herrero (javier.herrero@ucl.ac.uk) - added Sept 2014 - all new in BC 2.14
 
-## # For Javier Herrero (javier.herrero@ucl.ac.uk) - added Sept 2014 - all new in BC 2.14
+BiocManager::install ("DNAcopy", type="source", lib=mainLib);
+BiocManager::install ("Ringo", type="source", lib=mainLib);
+BiocManager::install ("affxparser", type="source", lib=mainLib);
+BiocManager::install ("aroma.light", type="source", lib=mainLib);
 
-## biocLite (pkgs="DNAcopy", type="source", lib=mainLib);
-## biocLite (pkgs="Ringo", type="source", lib=mainLib);
-## biocLite (pkgs="affxparser", type="source", lib=mainLib);
-## biocLite (pkgs="aroma.light", type="source", lib=mainLib);
+install.packages ("aroma.apd", lib=mainLib, repos=repros);
+install.packages ("aroma.affymetrix", lib=mainLib, repos=repros);
 
-## install.packages ("aroma.apd", lib=mainLib, repos=repros);
-## install.packages ("aroma.affymetrix", lib=mainLib, repos=repros);
+BiocManager::install ("Repitools", type="source", lib=mainLib);
 
-## biocLite (pkgs="Repitools", type="source", lib=mainLib);
+# For Dapeng Wang (dapeng.wang@ucl.ac.uk) - added Sept 2014
 
-## # For Dapeng Wang (dapeng.wang@ucl.ac.uk) - added Sept 2014
+BiocManager::install ("Rsubread", type="source", lib=mainLib);
+BiocManager::install ("DESeq", type="source", lib=mainLib);
+BiocManager::install ("genomeIntervals", type="source", lib=mainLib);
+BiocManager::install ("ShortRead", type="source", lib=mainLib);
+BiocManager::install ("easyRNASeq", type="source", lib=mainLib);
+BiocManager::install ("DESeq2", type="source", lib=mainLib);
 
-## biocLite (pkgs="Rsubread", type="source", lib=mainLib);
-## biocLite (pkgs="DESeq", type="source", lib=mainLib);
-## biocLite (pkgs="genomeIntervals", type="source", lib=mainLib);
-## biocLite (pkgs="ShortRead", type="source", lib=mainLib);
-## biocLite (pkgs="easyRNASeq", type="source", lib=mainLib);
-## biocLite (pkgs="DESeq2", type="source", lib=mainLib);
+# For Ran Gu (ran.gu.10@ucl.ac.uk) - added Oct 2014
 
-## # For Ran Gu (ran.gu.10@ucl.ac.uk) - added Oct 2014
+BiocManager::install ("RcppGSL", type="source", lib=mainLib);
 
-## biocLite (pkgs="RcppGSL", type="source", lib=mainLib);
+# For Stuart Peters (stuart.peters.13@ucl.ac.uk) - added Feb 2015
 
-## # For Stuart Peters (stuart.peters.13@ucl.ac.uk) - added Feb 2015
+BiocManager::install ("pls", type="source", lib=mainLib);
 
-## biocLite (pkgs="pls", type="source", lib=mainLib);
+# For Xiayi Ke (sejjxke@ucl.ac.uk) - added Feb 2015
 
-## # For Xiayi Ke (sejjxke@ucl.ac.uk) - added Feb 2015
+BiocManager::install ("registry", type="source", lib=mainLib);
+BiocManager::install ("pkgmaker", type="source", lib=mainLib);
+BiocManager::install ("rngtools", type="source", lib=mainLib);
+BiocManager::install ("doRNG", type="source", lib=mainLib);
+BiocManager::install ("bumphunter", type="source", lib=mainLib);
+BiocManager::install ("beanplot", type="source", lib=mainLib);
+BiocManager::install ("nor1mix", type="source", lib=mainLib);
+BiocManager::install ("siggenes", type="source", lib=mainLib);
+BiocManager::install ("minfi", type="source", lib=mainLib);
+BiocManager::install ("ChAMPdata", type="source", lib=mainLib);
 
-## biocLite (pkgs="registry", type="source", lib=mainLib);
-## biocLite (pkgs="pkgmaker", type="source", lib=mainLib);
-## biocLite (pkgs="rngtools", type="source", lib=mainLib);
-## biocLite (pkgs="doRNG", type="source", lib=mainLib);
-## biocLite (pkgs="bumphunter", type="source", lib=mainLib);
-## biocLite (pkgs="beanplot", type="source", lib=mainLib);
-## biocLite (pkgs="nor1mix", type="source", lib=mainLib);
-## biocLite (pkgs="siggenes", type="source", lib=mainLib);
-## biocLite (pkgs="minfi", type="source", lib=mainLib);
-## biocLite (pkgs="ChAMPdata", type="source", lib=mainLib);
+BiocManager::install ("Illumina450ProbeVariants.db", type="source", lib=dbLib);
 
-## biocLite (pkgs="Illumina450ProbeVariants.db", type="source", lib=dbLib);
+BiocManager::install ("sva", type="source", lib=mainLib);
 
-## biocLite (pkgs="sva", type="source", lib=mainLib);
+BiocManager::install ("IlluminaHumanMethylation450kmanifest", type="source", lib=dbLib);
 
-## biocLite (pkgs="IlluminaHumanMethylation450kmanifest", type="source", lib=dbLib);
+BiocManager::install ("methylumi", type="source", lib=mainLib);
+BiocManager::install ("nleqslv", type="source", lib=mainLib);
+BiocManager::install ("lumi", type="source", lib=mainLib);
 
-## biocLite (pkgs="methylumi", type="source", lib=mainLib);
-## biocLite (pkgs="nleqslv", type="source", lib=mainLib);
-## biocLite (pkgs="lumi", type="source", lib=mainLib);
+# BiocManager::install ("IlluminaHumanMethylation450k.db", type="source", lib=dbLib);
+BiocManager::install ("FDb.InfiniumMethylation.hg19", type="source", lib=mainLib);
+BiocManager::install ("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", lib=mainLib); # Added April 2019
 
-## # biocLite (pkgs="IlluminaHumanMethylation450k.db", type="source", lib=dbLib);
-## biocLite (pkgs="FDb.InfiniumMethylation.hg19", type="source", lib=mainLib);
+BiocManager::install ("ROC", type="source", lib=mainLib);
+BiocManager::install ("wateRmelon", type="source", lib=mainLib);
 
-## biocLite (pkgs="ROC", type="source", lib=mainLib);
-## biocLite (pkgs="wateRmelon", type="source", lib=mainLib);
+# Removed from CRAN and Bioconductor for R 3.4.0
+#
+# BiocManager::install ("RPVM", type="source", lib=mainLib);
 
-## # Removed from CRAN and Bioconductor for R 3.4.0
-## #
-## # biocLite (pkgs="RPVM", type="source", lib=mainLib);
+install.packages ("ruv", lib=mainLib, repos=repros);
+BiocManager::install ("ChAMP", type="source", lib=mainLib);
 
-## install.packages ("ruv", lib=mainLib, repos=repros);
-## biocLite (pkgs="ChAMP", type="source", lib=mainLib);
+# For Ana Paula Leite (ana.leite@ucl.ac.uk) - added June 2016
 
-## # For Ana Paula Leite (ana.leite@ucl.ac.uk) - added June 2016
+BiocManager::install ("EBSeq", type="source", lib=mainLib);
 
-## biocLite (pkgs="EBSeq", type="source", lib=mainLib);
+# Required for the cancerit suite (https://github.com/cancerit)
 
-## # Required for the cancerit suite (https://github.com/cancerit)
+BiocManager::install ("copynumber", type="source", lib=mainLib);
 
-## biocLite (pkgs="copynumber", type="source", lib=mainLib);
+# For Athina Dritsoula (athina.dritsoula.09@ucl.ac.uk) - added Feb and March 2017
 
-## # For Athina Dritsoula (athina.dritsoula.09@ucl.ac.uk) - added Feb and March 2017
+BiocManager::install ("goseq", type="source", lib=mainLib);
+BiocManager::install ("missMethyl", type="source", lib=dbLib);
 
-## biocLite (pkgs="goseq", type="source", lib=mainLib);
-## biocLite (pkgs="missMethyl", type="source", lib=dbLib);
+# For Evgeny Galimov (e.galimov@ucl.ac.uk) - added Aug 2017
 
-## # For Evgeny Galimov (e.galimov@ucl.ac.uk) - added Aug 2017
-
-## biocLite (pkgs="DEXSeq", type="source", lib=mainLib);
+BiocManager::install ("DEXSeq", type="source", lib=mainLib);
 
 # End of Bioconductor_UCL_1
 
-# biocLite (pkgs="XXX", type="source", lib=mainLib);
+# BiocManager::install ("XXX", type="source", lib=mainLib);
 
