75350376074adc559d8f1e9224cb86d94d7f28db
Brian Alston <b.alston@ucl.ac.uk>
2023-03-09 15:30:06 +0000

    CuDNN 8.1.0 for CUDA 11.2

---
+++ cudnn-8.1.0_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+###############################################
+# Installing Nvidia cuDNN library
+#
+# by Owain Kenway, 2017
+# Updated for version 8.1.0 March 2023 for CUDA 11.2
+
+# Library insatller needs to be downloaded from https://developer.nvidia.com/rdp/cudnn-download
+# using a Nvidia Developer login.
+
+NAME=${NAME:-cudnn}
+VERSION=${VERSION:-8.1.0}
+CUDA_VERSION=${CUDA_VERSION:-11.2}
+INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$CUDA_VERSION}
+
+SRC_ARCHIVE=${SRC_ARCHIVE:-/shared/ucl/apps/cudnn/installers/cudnn-${CUDA_VERSION}-linux-x64-v${VERSION}.tgz}
+
+set -e
+
+mkdir -p $INSTALL_PREFIX
+cd $INSTALL_PREFIX
+
+tar -xvf $SRC_ARCHIVE
