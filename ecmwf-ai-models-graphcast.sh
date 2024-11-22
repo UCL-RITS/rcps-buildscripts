1c72048627b5984a754cbc350412b3b2b041b5fb
Owain Kenway <o.kenway@ucl.ac.uk>
2024-11-22 10:39:46 +0000

    Actually commit script

--- ecmwf-ai-models-graphcast.sh
+++ ecmwf-ai-models-graphcast.sh
@@ -1,47 +1,45 @@
 #!/usr/bin/env bash
 
 ###############################################
-# Installing 
+# Installing the ECMWF ai-models/graphcast package
 #
-# by Owain Kenway, 2018
+# by Owain Kenway, 2024
 #
 set -e
 
 for i in ${includes_dir:=$(dirname $0 2>/dev/null)/includes}/{module_maker,require}_inc.sh; do . $i; done
 
 require gcc-libs/4.9.2
+require openssl/1.1.1u
+require python/3.11.4
 
-NAME=${NAME:-}
-VERSION=${VERSION:-}
-INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$COMPILER_TAG}
-MD5=${MD5:-}
-SHA1=${SHA1:-}
-SHA256=${SHA256:-}
-SHA512=${SHA512:-}
+NAME=${NAME:-ai-models-graphcast}
+VERSION=${VERSION:-0.1.0}
+INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION}
 
-SRC_ARCHIVE=${SRC_ARCHIVE:-}
+rm -rf ${INSTALL_PREFIX}/venv
+mkdir -p ${INSTALL_PREFIX}
 
+virtualenv ${INSTALL_PREFIX}/venv
+source ${INSTALL_PREFIX}/venv/bin/activate
 
-mkdir -p /dev/shm/$(whoami)/${NAME}
+# Get pip and setuptools up to date
+pip install --upgrade pip setuptools wheel
 
-temp_dir=$(mktemp -d -p /dev/shm/$(whoami)/${NAME})
+# Install the right "old" version of Jax from the google mirror along with pre 2.0 Numpy
+pip install jax[cuda11_pip]==0.4.23 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html "numpy<2"
 
-cd $temp_dir
+# Downgrade to the right "old" version of cudnn
+pip install nvidia-cudnn-cu11==8.6.0.163
 
-wget $SRC_ARCHIVE
-archive=$(basename "${SRC_ARCHIVE}")
+# Install Deepmind's Graphcast, forcing an old version of chex which is compatible with it.
+pip install git+https://github.com/deepmind/graphcast.git chex==0.1.83
 
-md5sum -c <<< "$MD5 $archive"
-sha1sum -c <<< "$SHA1 $archive"
-sha256sum -c <<< "$SHA256 $archive"
-sha512sum -c <<< "$SHA512 $archive"
+# Install ai-models packages
+pip install ai-models ai-models-graphcast==${VERSION}
 
-tar -xvf $archive
+# Install climetlab so that they can fetch the data
+pip install climetlab
 
-cd ${NAME}-${VERSION}
-
-./configure --prefix=$INSTALL_PREFIX
-make 
-make install
-
-rm -rf $temp_dir
+# Fix earthkit so downloads from CDS work
+pip install "earthkit-data>=0.11.1"
